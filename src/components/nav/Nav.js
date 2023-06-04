bd61eecf1ec68d616139035e4249a13fe77e1026
Hassan Abdellah <hassanabdellah25@gmail.com>
2023-06-04 19:49:24 +0300

    removing unused imports

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-scroll";
 import Social from "../social/Social";
 import Toggle from "../toggle/Toggle";
